f1dc7f555ff8b4caeb64c00f9d15c81bc0c4fe00
Pâyidar <79402324+payidarofficial@users.noreply.github.com>
2021-03-07 12:56:05 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+# Öncelikle Dictionary Oluştualım
+dict2 = {
+    'isim': 'Kadir',
+    'yas': 23,
+    'dogdugu_sehir': 'Angara',
+    'yasadigi_ilce': 'Altındağ',
+}
+
+# Ekrana Yazdıralım
+print(dict1)
+
+
+    # Sonucumuzu Gördük Dostlar, Şimdi İse Bir Adet Örnek Verelim
+    
+print('Kullanıcının İsmi', dict2.get('isim'), 'Yaşadığı İlçe', dict2.get('yasadigi_ilce'))
+
+  # Gördüğünüz Gibi :).
